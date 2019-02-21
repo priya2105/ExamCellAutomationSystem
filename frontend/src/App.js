89da2580a3e0ac1bdc968443d209d65319a28e08
priya2105 <36656333+priya2105@users.noreply.github.com>
2019-02-22 01:10:36 +0530

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from './login';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        }}
+        render(){
+         return(
+      <div>
+        <Login/>
+      </div>
+    );
+  }
+}
+export default App;
+
